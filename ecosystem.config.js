ce98933a5cfd6a81ce1658a67260d946199d9d49
alvin <alvinecom2018@gmail.com>
2024-06-19 18:14:52 +0700

    add ecosystem

---
+++ ecosystem.config.js
@@ -0,0 +1,19 @@
+// ecosystem.config.js
+module.exports = {
+    apps: [
+      {
+        name: 'nextjs-app',
+        script: 'npm',
+        args: 'run dev',
+        instances: 1,
+        autorestart: true,
+        watch: false,
+        max_memory_restart: '1G',
+        env: {
+          NODE_ENV: 'development',
+          PORT: 3001, // Adjust port if needed
+        },
+      },
+    ],
+  };
+  
