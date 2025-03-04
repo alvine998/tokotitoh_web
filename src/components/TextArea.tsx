5e46b25471d8a2374760f133dc8a7680f2744cbc
alvin <alvinecom2018@gmail.com>
2025-03-05 06:00:01 +0700

    --

--- src/components/TextArea.tsx
+++ src/components/TextArea.tsx
@@ -13,7 +13,7 @@ export default function TextArea(props: Props) {
                 <label htmlFor={label} className={'text-gray-500 ' + props.className}>{label}</label>
             }
             <textarea id={label} {...props}
-                className={"block w-full rounded-md border-0 py-1.5 pl-4 pr-2 h-[150px] text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-500 focus:outline-none sm:leading-6 " + props.className}
+                className={"block w-full rounded-md border-0 py-1.5 pl-4 pr-2 h-[250px] text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-500 focus:outline-none sm:leading-6 " + props.className}
             />
         </div>
     )
